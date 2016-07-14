6ec43e005eb2c1d91f78d7809e3e87b360a5f894
Ben <bhturne@g.clemson.edu>
2016-07-14 11:28:58 -0400

    Initial commit

---
+++ src/ants_clojure/core.clj
@@ -0,0 +1,21 @@
+(ns ants-clojure.core  
+  (:require [clojure.java.io :as io])  
+  (:gen-class :extends javafx.application.Application))
+
+(def width 800)
+(def height 600)
+(def ant-count 100)
+(def ants (atom []))
+
+(defn draw-ants)
+
+(defn -start [app stage]  
+  (let [root (javafx.fxml.FXMLLoader/load (io/resource "main.fxml"))       
+        scene (javafx.scene.Scene. root width height)]   
+    (.setTitle stage "Ants!!!")   
+    (.setScene stage scene)   
+    (.show stage)))
+
+
+(defn -main []  
+  (javafx.application.Application/launch ants_clojure.core (into-array String [])))  ;launches javafx
